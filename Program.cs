eed5657277a226fb6ca5d7f6a80287fe75a233c2
ahronSteinbruch <139044285+ahronSteinbruch@users.noreply.github.com>
2025-05-12 14:42:38 +0300

    get Numbers function

--- Program.cs
+++ Program.cs
@@ -8,8 +8,111 @@ namespace _9200
 {
     internal class Program
     {
+        static int length = 0;
+        static int sum = 0;
+        static int[] numbers;
+
+        static void merge(int[] numbers, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            // Create temp arrays
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = numbers[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = numbers[m + 1 + j];
+
+            // Merge the temp arrays
+
+            // Initial indexes of first and second subarrays
+            i = 0;
+            j = 0;
+
+            // Initial index of merged
+            // subarray array
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    numbers[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    numbers[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements of L[] if any
+            while (i < n1)
+            {
+                numbers[k] = L[i];
+                i++;
+                k++;
+            }
+
+            // Copy remaining elements of R[] if any
+            while (j < n2)
+            {
+                numbers[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        static void mergeSort(int[] numbers, int l, int r)
+        {
+            if (l < r)
+            {
+
+                // Find the middle point
+                int m = l + (r - l) / 2;
+
+                // Sort first and second halves
+                mergeSort(numbers, l, m);
+                mergeSort(numbers, m + 1, r);
+
+                // Merge the sorted halves
+                merge(numbers, l, m, r);
+            }
+        }
+
+
+        //return the series
+        static int[] Display()
+        {
+            return numbers; 
+        }
+        //return the series in revers order
+        static int[] DisplayRevers()
+        {
+            int[] revers = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+               revers[i] =  numbers[length - i - 1];
+            }
+            return revers;
+        }
+        
+        static int[] Sort()
+        {
+            int[] sorted = new int[length];
+            for(int i = 0; i < length;) sorted[i] = numbers[i];
+            s
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
